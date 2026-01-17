8514888bdad399c86ee6394243c87d8619cb027d
shaishab316 <shaishabchandrashil@gmail.com>
2026-01-18 03:39:22 +0600

    refactor: reorganize static file serving and clean up unused code in routes

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -3,9 +3,7 @@ import auth from '../app/middlewares/auth';
 import AdminRoutes from '../app/modules/admin/Admin.route';
 import { AuthRoutes } from '../app/modules/auth/Auth.route';
 import { UserRoutes } from '../app/modules/user/User.route';
-import { StatusCodes } from 'http-status-codes';
 import { ContextPageRoutes } from '../app/modules/contextPage/ContextPage.route';
-import { fileTypes } from '../app/middlewares/capture';
 import { DriverRoutes } from '../app/modules/driver/Driver.route';
 import { TripRoutes } from '../app/modules/trip/Trip.route';
 import { ChatRoutes } from '../app/modules/chat/Chat.route';
@@ -17,19 +15,7 @@ import { NotificationRoutes } from '../app/modules/notification/Notification.rou
 import { ReferRoutes } from '../app/modules/refer/Refer.route';
 import { AvailableLoanRoutes } from '../app/modules/availableLoan/AvailableLoan.route';
 
-const appRouter = Router();
-
-/** Forward uploaded files requests */
-fileTypes.map((filetype: string) =>
-  appRouter.get(`/${filetype}/:filename`, (req, res) =>
-    res.redirect(
-      StatusCodes.MOVED_PERMANENTLY,
-      `/${filetype}/${encodeURIComponent(req.params.filename)}`,
-    ),
-  ),
-);
-
-export default injectRoutes(appRouter, {
+export default injectRoutes(Router(), {
   '/context-pages': [ContextPageRoutes.user],
 
   // No auth
