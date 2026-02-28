8ace7a40a958a5b7d65ad2a5f3c471eefb550aad
shaishab316 <shaishabchandrashil@gmail.com>
2026-02-28 10:17:56 +0600

    feat: enhance catchAsync and purifyRequest middlewares with improved type handling and error management

--- src/app/middlewares/purifyRequest.ts
+++ src/app/middlewares/purifyRequest.ts
@@ -1,14 +1,8 @@
 /* eslint-disable no-unused-vars */
-import type { Request } from 'express';
 import { ZodObject } from 'zod';
 import catchAsync from './catchAsync';
-import config from '../../config';
 
 const keys = ['body', 'query', 'params', 'cookies'] as const;
-
-type SchemaOrFn =
-  | ZodObject
-  | ((req: Request) => ZodObject | Promise<ZodObject>);
 
 /**
  * Middleware to validate and sanitize incoming Express requests using Zod schemas.
@@ -16,34 +10,36 @@ type SchemaOrFn =
  * Supports static and dynamic schemas (functions returning schemas).
  * Validates body, query, params, and cookies, then merges results into `req`.
  */
-const purifyRequest = (...schemas: SchemaOrFn[]) =>
-  catchAsync(
-    async (req, _, next) => {
-      const results = await Promise.all(
-        schemas.map(async schema => {
-          const zodSchema =
-            typeof schema === 'function' ? await schema(req) : schema;
-          return zodSchema.parseAsync(req);
-        }),
+const purifyRequest = (...schemas: ZodObject[]) =>
+  catchAsync(async (req, _, next) => {
+    keys.forEach(key => {
+      req[key] ??= {}; // Ensure the property exists on req
+    });
+
+    const results = await Promise.all(
+      schemas.map(async schema => schema.parseAsync(req)),
+    );
+
+    keys.forEach(key => {
+      const purified = Object.assign(
+        {},
+        ...results.map((result: any) => result?.[key] ?? {}),
       );
 
-      keys.forEach(key => {
-        req[key] = Object.assign(
-          {},
-          key === 'params' && req.params,
-          ...results.map((result: any) => result?.[key] ?? {}),
-        );
+      //? Fix express 5 issue (req is read-only)
+      Object.defineProperty(req, key, {
+        value: purified,
+        writable: true,
+        configurable: true,
+        enumerable: true,
       });
+    });
 
-      next();
-    },
-    (error, req, _, next) => {
-      if (config.server.isDevelopment)
-        // eslint-disable-next-line no-console
-        keys.forEach(key => console.log(`${key} :`, req[key]));
+    next();
 
-      next(error);
-    },
-  );
+    if (process.env.NODE_ENV !== 'production')
+      // eslint-disable-next-line no-console
+      keys.forEach(key => console.log(`${key} :`, req[key]));
+  });
 
 export default purifyRequest;
